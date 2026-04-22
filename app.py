17a00becde10ae6dc64eb8a5c4edf11776731884
manvk8-bit <manvk8@gmail.com>
2026-04-22 18:32:59 +0530

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI
 
