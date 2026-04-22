5b363813cc41387542d386eaef5c7dcd9f4e1f05
manvk8-bit <manvk8@gmail.com>
2026-04-22 17:21:16 +0530

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 
